ad7fec53ecffd8acfdc8b56b94ca208c66fe4c01
sazima <w@wktadmin.com>
2020-05-03 23:26:57 +0800

    notice

--- code/src/main/java/q31/Solution.java
+++ code/src/main/java/q31/Solution.java
@@ -19,6 +19,7 @@ import com.sun.xml.internal.bind.v2.runtime.SwaRefAdapter;
 链接：https://leetcode-cn.com/problems/next-permutation
 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
  */
+// 方法2好理解点
 class Solution {
     public void nextPermutation(int[] nums) {
         int i = nums.length - 2;
