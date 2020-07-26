a50a15a0b61e44066ca687befa90bc6197e02d23
sazima <w@wktadmin.com>
2020-07-26 11:39:25 +0800

    141

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # leetcode
 code
+
+# todo:
+- q139 看不懂题: https://leetcode-cn.com/problems/word-break/comments/
---
+++ java_code/src/main/java/q139/Solution.java
@@ -0,0 +1,44 @@
+package q139;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+
+
+/*
+139. 单词拆分
+给定一个非空字符串 s 和一个包含非空单词列表的字典 wordDict，判定 s 是否可以被空格拆分为一个或多个在字典中出现的单词。
+
+说明：
+
+拆分时可以重复使用字典中的单词。
+你可以假设字典中没有重复的单词。
+示例 1：
+
+输入: s = "leetcode", wordDict = ["leet", "code"]
+输出: true
+解释: 返回 true 因为 "leetcode" 可以被拆分成 "leet code"。
+示例 2：
+
+输入: s = "applepenapple", wordDict = ["apple", "pen"]
+输出: true
+解释: 返回 true 因为 "applepenapple" 可以被拆分成 "apple pen apple"。
+     注意你可以重复使用字典中的单词。
+示例 3：
+
+输入: s = "catsandog", wordDict = ["cats", "dog", "sand", "and", "cat"]
+输出: false
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/word-break
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class Solution {
+    public boolean wordBreak(String s, List<String> wordDict) {
+        return false;
+    }
+
+
+    public static void main(String[] args) {
+    }
+}
