8608287950ee7434bfe61aead29602de6d9950bc
sazima <w@wktadmin.com>
2022-04-12 20:01:49 +0800

    53

--- python_code/q50/s.py
+++ python_code/q50/s.py
@@ -39,8 +39,8 @@ class Solution:
             return 1 / self.myPow(x, -n)
 
     def _myPow(self, x: float, n: int) -> float:
-        if n == 0:
-            return 1
+
+
         if n == 1:
             return x
         if n % 2 == 0:
