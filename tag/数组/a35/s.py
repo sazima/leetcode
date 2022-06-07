7819403dc9ed657c3abb6d67d4c80c61f5d87c71
sazima <w@wktadmin.com>
2022-06-07 19:49:03 +0800

    35

---
+++ tag/数组/a35/s.py
@@ -0,0 +1,55 @@
+"""
+35. 搜索插入位置
+给定一个排序数组和一个目标值，在数组中找到目标值，并返回其索引。如果目标值不存在于数组中，返回它将会被按顺序插入的位置。
+
+请必须使用时间复杂度为 O(log n) 的算法。
+
+
+
+示例 1:
+
+输入: nums = [1,3,5,6], target = 5
+输出: 2
+示例 2:
+
+输入: nums = [1,3,5,6], target = 2
+输出: 1
+示例 3:
+
+输入: nums = [1,3,5,6], target = 7
+输出: 4
+
+
+提示:
+
+1 <= nums.length <= 104
+-104 <= nums[i] <= 104
+nums 为 无重复元素 的 升序 排列数组
+-104 <= target <= 104
+"""
+from typing import List
+
+
+class Solution:
+    def searchInsert(self, nums: List[int], target: int) -> int:
+        left = 0
+        right = len(nums) - 1
+        while left < right:
+            mid = (left + right) // 2
+            if nums[mid] == target:
+                return mid
+            elif nums[mid] > target:
+                right = mid - 1
+            else:
+                left = mid + 1
+        if nums[left] < target:
+            return left + 1
+        return left
+
+
+if __name__ == '__main__':
+    nums = [1,3,5,6]
+    target = 2
+
+    res = Solution().searchInsert(nums, target)
+    print(res)
